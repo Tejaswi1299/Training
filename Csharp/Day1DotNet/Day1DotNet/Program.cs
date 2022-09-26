f37332d5771516fe2b2fca1574c9133d79f57b2e
Pagadala Tejaswi <pagadalat@infinite.com>
2022-09-26 21:55:29 +0530

    Adding modification

--- Csharp/Day1DotNet/Day1DotNet/Program.cs
+++ Csharp/Day1DotNet/Day1DotNet/Program.cs
@@ -15,8 +15,15 @@ namespace Day1DotNet
             //int Result = program.Add2nos(5, 10);
             //Console.WriteLine(Result);
             Console.WriteLine(Add2nos(10, 10));
-            TestClass TC = new TestClass();
+            TestClass tc = new TestClass();
+             tc.show();
+            tc.accept();
+            tc.show();
+            // TestClass tc2 = new TestClass();
+            // tc2.Show();
+            TestClass.Message();
             Console.ReadKey();
+            
 
         }
 
@@ -34,11 +41,21 @@ namespace Day1DotNet
 
         public void show()
         {
-            Console.WriteLine(age + " ", name);
+            Console.WriteLine(age + " " + name + " " + companyname); //concatenation
+            Console.WriteLine("Name is :{0} Age is :{1} and Company Name is :{2}", name, age, companyname); //place holder
+            Console.WriteLine($"Name is {name}, Age is {age},Company name is {companyname}");
         }
-        static void accept()
+        public void accept()
         {
-            Console.WriteLine("Enter age and name")
+            Console.WriteLine("Enter age, name, company name");
+            age = Convert.ToInt32(Console.ReadLine());
+            name = Console.ReadLine();
+            companyname = Console.ReadLine();
+
+        }
+        public static string Message()  //static method are maintained at class level
+        {
+            return "Hello";
         }
     }
 
---
+++ Csharp/Temparature in Celsius and Fahreheit/Temparature in Celsius and Fahreheit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temparature_in_Celsius_and_Fahreheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius = 40;
+
+            double Fahrenheit;
+
+            //Console.WriteLine("Celsius:" + Celsius);
+            Fahrenheit = ((Celsius * 9) / 5) + 32;
+            //Console.WriteLine( "Fahrenheit:" + Fahrenheit);
+            
+            Console.WriteLine("Celsius :{0} Fahrenheit :{1}", Celsius, Fahrenheit);
+            Console.ReadLine();
+
+        }
+    }
+}
