36dc56435426e67a4c3d31a03d462a8abb299cb2
Pagadala Tejaswi <pagadalat@infinite.com>
2022-09-28 00:29:20 +0530

    adding one more program to Day1

---
+++ Csharp/AvgOf5Students/AvgOf5Students/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvgOf5Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Csharp;
+            int html;
+            int sql;
+            int total;
+
+            for (int i = 1; i <= 5; i++)
+            {
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.WriteLine("Enter Csharp marks for Student " + i);
+                    Csharp = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter html marks for Student " + i);
+                    html = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter sql marks for Student" + i);
+                    sql = Convert.ToInt32(Console.ReadLine());
+                    total = Csharp + html + sql;
+                    total = total / 3;
+
+                    if (total < 50)
+                    {
+                        Console.WriteLine("total  mark for student " + i + " is" + total);
+                        Console.WriteLine("student {0} is Failed", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("total  mark for student " + i + " is" + total);
+                        Console.WriteLine("student {0} is passed", i);
+                    }
+
+                }
+
+            }
+        }
+    }
+}
