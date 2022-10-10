e08d30fb5b16f94e2527dea33be2ef4a9b0ee227
Pagadala Tejaswi <pagadalat@infinite.com>
2022-10-11 01:36:24 +0530

    Assignment 5

---
+++ Csharp/Assignment5/Assignment5/Assignment5/Passanger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 2. Create an console application to book train tickets. Create a Passanger class with (Name, Age) and write a function called 
+      TicketBooking(int no_of_tickets) that takes no.of tickets to be booked. If the no of tickets is > 2 per booking, 
+      raise an user defined exception, and print "cannot book more than 2 tickets". Else Print  "Ticket Booked Successfully". 
+      Add a Test class to call TicketBooking method by accepting all required details.  */
+
+namespace Assignment5
+{
+    class ticketsBookingException : ApplicationException
+    {
+        public ticketsBookingException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+    class Passanger
+    {
+        public int Age;
+        public int no_of_tickets;
+        public string Name;
+
+        public void AcceptPassengerDetails()
+        {
+            Console.WriteLine("Enter the Number of tickets : ");
+            no_of_tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the name of Passanger : ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Age :");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            TicketBooking(no_of_tickets);
+        }
+
+        public void TicketBooking(int no_of_tickets)
+        {
+            this.no_of_tickets = no_of_tickets;
+
+            if (no_of_tickets > 2)
+            {
+                throw (new ticketsBookingException("cannot book more than 2 tickets"));
+            }
+            else
+            {
+                Console.WriteLine("Ticket Booked Successfully");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passanger objct = new Passanger();
+
+            try
+            {
+                objct.AcceptPassengerDetails();
+            }
+            catch (ticketsBookingException te)
+            {
+                Console.WriteLine(te.Message);
+            }
+            Console.Read();
+        }
+    }
+}
+
---
+++ Csharp/Assignment5/Assignment5/Assignment5/StudentDetails.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 4.Create an Interface IStudent with int StudentId and string Name as Properties, void ShowDetails() as its method.
+
+    Create 2 classes Dayscholar and Resident that implements the interface Properties and Methods.ShowDetails() should show
+    DaysScholar details and Resident Details accordingly.  */
+
+namespace Assignment5
+{
+    class StudentDetails
+    {
+        interface IStudent
+        {
+            int StudentId { get; set; }
+            string Name { get; set; }
+            void ShowDetatils();
+        }
+
+        public class DayScholar : IStudent
+        {
+            public int StudentId { get; set; }
+            public string Name { get; set; }
+
+            public int studentId
+            {
+                set { StudentId = value; }
+                get { return StudentId; }
+            }
+
+            public string name
+            {
+                set { Name = value; }
+                get { return Name; }
+            }
+
+            public void GetDayScholar_StudentDetails()
+            {
+                Console.Write("Enter DayScholar Student Id :");
+                studentId = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter DayScholar Student Name : ");
+                name = Console.ReadLine();
+            }
+
+            public void ShowDetatils()
+            {
+                Console.WriteLine(" Day Scholar Student Details  ");
+                Console.WriteLine("StudentId : {0}\nName : {1}", StudentId, Name);
+            }
+        }
+
+        public class Resident : IStudent
+        {
+            public int StudentId { get; set; }
+            public string Name { get; set; }
+
+            public int studentId
+            {
+                set { StudentId = value; }
+                get { return StudentId; }
+            }
+
+            public string name
+            {
+                set { Name = value; }
+                get { return Name; }
+            }
+
+            public void GetResident_StudentDetails()
+            {
+                Console.Write("Enter Resident Student Id :");
+                studentId = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Resident Student Name : ");
+                name = Console.ReadLine();
+            }
+
+            public void ShowDetatils()
+            {
+                Console.WriteLine("  Resident Student Details ");
+                Console.WriteLine("StudentId : {0}\nName : {1}", StudentId, Name);
+            }
+        }
+
+        class IStudent_Interface
+        {
+            public static void Main(string[] args)
+            {
+                Resident r1 = new Resident();
+                r1.GetResident_StudentDetails();
+                r1.ShowDetatils();
+
+                Console.WriteLine("----------------------------");
+
+                DayScholar d1 = new DayScholar();
+                d1.GetDayScholar_StudentDetails();
+                d1.ShowDetatils();
+              Console.ReadKey();
+            }
+        }
+
+    }
+}
